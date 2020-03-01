b8faa21df73f155f369c45ffedab21c5f25d3e95
sugita-seiya <satreu16@ezweb.ne.jp>
2020-03-01 10:25:53 +0900

    deviseのビュー変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_025635) do
+ActiveRecord::Schema.define(version: 2020_03_01_010748) do
 
   create_table "attendances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "days"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_025635) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
