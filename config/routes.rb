e8c9cab2b45948af1d0f3a0aa8636d1f93f0050b
sugita-seiya <satreu16@ezweb.ne.jp>
2020-03-04 15:59:47 +0900

    不要なルーティンングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root to: "top#index"
-  # devise_for :installs
   get 'attendances/index'
   get 'attendances/edit'
   resources :groups, only: [:index,:new,:create]
-  devise_for :installs
   root to: "top#index"
   devise_for :users
   resources :reports do
