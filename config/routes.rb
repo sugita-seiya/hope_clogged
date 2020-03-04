be2db746287e3a30a442276432a5e59a2186d78a
sugita-seiya <satreu16@ezweb.ne.jp>
2020-03-04 16:06:38 +0900

    ルーティングが２つあったので削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   get 'attendances/index'
   get 'attendances/edit'
   resources :groups, only: [:index,:new,:create]
-  root to: "top#index"
   devise_for :users
   resources :reports do
     collection do
       get 'top'
     end
   end
-  devise_for :users
   resources :attendances, only: [:index,:edit,:update]
 end
