0e39616655d2b13a16fb6163bbfbcf17e2f932fa
sugita-seiya <satreu16@ezweb.ne.jp>
2020-03-05 14:01:16 +0900

    グループ一覧表示のマークアップ実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "top#index"
   get 'attendances/index'
   get 'attendances/edit'
-  resources :groups, only: [:index,:new,:create]
+  resources :groups, only: [:index,:new,:create,:edit]
   devise_for :users
   resources :reports do
     collection do
