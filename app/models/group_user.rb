8b72bff75468143ff178434f83a31f264a518194
sugita-seiya <satreu16@ezweb.ne.jp>
2020-03-21 06:27:54 +0900

    部署に所属出来ない問題を解決

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,4 +1,4 @@
 class GroupUser < ApplicationRecord
-  belongs_to :group, foreign_key: :group_id, dependent: :destroy
-  belongs_to :user, dependent: :destroy
+  belongs_to :group, foreign_key: :group_id
+  belongs_to :user, optional: true
 end
